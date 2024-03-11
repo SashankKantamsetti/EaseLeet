8187d4b6da747d42484d79f94ba9a28913f6cb8b
jogendr2018s <jogendraruppa@gmail.com>
2024-03-11 21:48:37 +0530

    Second Commit

--- background.js
+++ background.js
@@ -1,7 +1,13 @@
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-        console.log(tab);
+        chrome.tabs.sendMessage(tabId, { message: "yes-main" });
+        if (changeInfo.url && changeInfo.url.includes("https://leetcode.com/")) {
+            console.log("yes-main")
+            chrome.tabs.sendMessage(tabId, { message: "yes-main" });
+        }
         if (changeInfo.url && changeInfo.url.includes("https://leetcode.com/problems/")) {
             console.log("yes")
             chrome.tabs.sendMessage(tabId, { message: "yes" });
-    }
+        }
+        
 });
